abd6f106122e2dcfef3f3f59d6aa5e9be9b161df
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-03-14 07:52:37 -0300

    deleting initial structure and start a ddd structure. nstalling the swagger module from nest and setting up the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+// Nest imports
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger'
+
+// Files import
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
