efcd25840d605ce3ada4cf8996a5868bec6c9fc7
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-04-05 15:46:38 -0300

    set up user model in the db

---
+++ src/db/models/user.ts
@@ -0,0 +1,45 @@
+import { CreationOptional, Model } from "sequelize";
+import { Column, PrimaryKey, Table, DataType } from "sequelize-typescript";
+import { Col } from "sequelize/types/utils";
+
+@Table({
+  tableName: 'Users',
+  paranoid: true
+})
+
+export class User extends Model{
+  @PrimaryKey
+  @Column({ type: DataType.UUID })
+  public declare id: string;
+
+  @Column({ type: DataType.STRING })
+  public declare name: string;
+
+  @Column({ type: DataType.STRING })
+  public declare email: string;
+
+  @Column({ type: DataType.STRING })
+  public declare revenueRecord: string;
+
+  @Column({ type: DataType.STRING })
+  public declare role: string;
+
+  @Column({ type: DataType.STRING })
+  public declare phone: string;
+
+  @Column({ type: DataType.STRING })
+  public declare password: string;
+
+  @Column({ type: DataType.DATE })
+  public declare loginDate: Date;
+
+  @Column({ type: DataType.DATE })
+  public declare createdAt: CreationOptional<Date>;
+
+  @Column({ type: DataType.DATE })
+  public declare updatedAt: CreationOptional<Date>;
+
+  @Column({ type: DataType.DATE })
+  public declare deletedAt: CreationOptional<Date>;
+  
+}
