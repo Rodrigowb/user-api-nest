0eb054a77191c41aa6b1a8fdee7c47b045f3a732
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2023-04-05 15:32:49 -0300

    implementing role auth guard

--- src/cross-cutting/guards/role-auth.guard.ts
+++ src/cross-cutting/guards/role-auth.guard.ts
@@ -3,4 +3,18 @@ import { CanActivate, Inject, mixin } from "@nestjs/common";
 
 // Files import
 import { SessionService } from "src/services/session/session.service";
-import {AppConstants} from '../constants/app.constants'
+import { AppConstants } from '../constants/app.constants';
+
+export const RoleGuard: any = (roles: string[]) => {
+  class RoleGuardMixin implements CanActivate {
+      public constructor(@Inject(SessionService) private readonly session) {}
+
+      public canActivate() {
+          if (process.env.NODE_ENV.toString() === AppConstants.NODE_ENV.TEST) return true;
+          return roles.includes(this.session.getUser().role);
+      }
+  }
+
+  const guard: any = mixin(RoleGuardMixin);
+  return guard;
+};
